fa832342f811fdd36004fb0d01efa271d5a33ca5
ingyeo-tech <62136487+ingyeo-tech@users.noreply.github.com>
2020-04-30 00:01:26 +0900

    게임결과 화면 구성

---
+++ Unity 2D Rhythm Game/Assets/Scripts/GameResultManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameResultManager : MonoBehaviour
+{
+    public Text musicTitleUI;
+    public Text scoreUI;
+    public Text maxComboUI;
+
+    void Start()
+    {
+        musicTitleUI.text = PlayerInformation.musicTitle;
+        scoreUI.text = "" + PlayerInformation.score; //int 문자열로 변환하기! 
+        maxComboUI.text = "" + PlayerInformation.maxCombo;
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity 2D Rhythm Game/Assets/Scripts/PlayerInformation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerInformation
+{
+    //어디서든 접근가능 mono상속안함. 보안 높음. 프로퍼티 안함. 근데 왜?
+    public static int maxCombo { get; set; }
+    public static float score { get; set; }
+    public static string musicTitle { get; set; }
+    public static string musicArtist { get; set; }
+}
